b5a78d659d77fa795c54a0443a36ab38e42c7720
Stein Petter Tokvam <stto1210@gmail.com>
2024-09-05 09:50:06 +0200

    yield bb

--- routes/barebitcoin.js
+++ routes/barebitcoin.js
@@ -44,9 +44,9 @@ function setFeeAmount(item) {
 function setCost(item) {
     if (item.type === "BTC_WITHDRAWAL") {
         return parseFloat(parseFloat(item.outAmount * item.rateMarket).toFixed(2))
-    } else if(item.type === "BTC_DEPOSIT") {
+    } else if (item.type === "BTC_DEPOSIT") {
         return parseFloat(parseFloat(item.inAmount * item.rateMarket).toFixed(2))
-    } else if(item.type === "BTC_BUY") {
+    } else if (item.type === "BTC_BUY") {
         return parseFloat(parseFloat(item.outAmount).toFixed(2))
     } else if (item.type === "BTC_BONUS") {
         return parseFloat((parseFloat(item.rateMarket) * parseFloat(item.inAmount)).toFixed(2))
@@ -62,20 +62,11 @@ function getPrice(onlyPrice) {
         }
     })
         .then(response => response.json())
-        .then(response => onlyPrice ? response.buyBtcnok - (response.buyBtcnok - response.sellBtcnok) : response)
+        .then(response => onlyPrice ? response.midBtcnok : response)
 }
 
-router.get('/price', function (req, res, next) {
-    getPrice(false)
-        .then(response => res.send(response))
-});
-
-router.post('/balance', function (req, res, next) {
-    const { accessKey, accountKey } = req.body
-    if(accessKey === undefined) {
-        return next(createError(400, 'Missing required fields accessKey or accountKey'))
-    }
-    fetch('https://api.bb.no/export/balance', {
+function getTransactions(accessKey) {
+    return fetch('https://api.bb.no/export/transactions', {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${accessKey}`
@@ -83,37 +74,7 @@ router.post('/balance', function (req, res, next) {
     })
         .then(response => response.json())
         .then(response => {
-            getPrice(true)
-                .then(price => {
-                    res.send(response.bitcoinAccounts.filter(res => res.name === "Hovedkonto").map(account => {
-                        return {
-                            name: "BTC",
-                            accountKey: accountKey,
-                            equityShare: parseFloat(account.balanceBitcoin),
-                            equityType: "Cryptocurrency",
-                            value: price * parseFloat(account.balanceBitcoin),
-                            goalPercentage: 0,
-                            yield: 0,
-                        }
-                    }))
-                })
-        })
-})
-
-router.post('/transactions', function (req, res, next) {
-    const { accessKey } = req.body
-    if(accessKey === undefined) {
-        return next(createError(400, 'Missing required fields accessKey'))
-    }
-    fetch('https://api.bb.no/export/transactions', {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessKey}`
-        }
-    })
-        .then(response => response.json())
-        .then(response => {
-            const filteredData = response
+            return response
                 .filter(item => item.inCurrency !== "NOK")
                 .filter(item => item.accountId !== "acc_01J631DK3N56K40P6NC1HZXWBQ")
                 .map(item => {
@@ -131,7 +92,61 @@ router.post('/transactions', function (req, res, next) {
                         fee: setFeeAmount(item)
                     };
                 });
-            res.send(filteredData)
+        })
+}
+
+router.get('/price', function (req, res, next) {
+    getPrice(false)
+        .then(response => res.send(response))
+});
+
+router.post('/balance', function (req, res, next) {
+    const { accessKey, accountKey } = req.body
+    if (accessKey === undefined) {
+        return next(createError(400, 'Missing required fields accessKey or accountKey'))
+    }
+    fetch('https://api.bb.no/export/balance', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessKey}`
+        }
+    })
+        .then(response => response.json())
+        .then(response => {
+            getPrice(true)
+                .then(price => {
+                    getTransactions(accessKey)
+                        .then(transactions => {
+                            res.send(response.bitcoinAccounts.filter(res => res.name === "Hovedkonto").map(account => {
+                                console.log(account.balanceBitcoin * price)
+                                return {
+                                    name: "BTC",
+                                    accountKey: accountKey,
+                                    equityShare: parseFloat(account.balanceBitcoin),
+                                    equityType: "Cryptocurrency",
+                                    value: price * parseFloat(account.balanceBitcoin),
+                                    goalPercentage: 0,
+                                    yield: (price * parseFloat(account.balanceBitcoin)) - transactions.reduce((a, b) => {
+                                        if (b.type === "WITHDRAWAL") {
+                                            return a - (b.cost - b.fee)
+                                        }
+                                        return a + b.cost
+                                    }, 0),
+                                }
+                            }))
+                        })
+                })
+        })
+})
+
+router.post('/transactions', function (req, res, next) {
+    const { accessKey } = req.body
+    if (accessKey === undefined) {
+        return next(createError(400, 'Missing required fields accessKey'))
+    }
+    getTransactions()
+        .then(transactions => {
+            res.send(transactions)
         })
 })
 
