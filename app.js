0ae8b3231b2adb448ccf92f8bfd451e41e41edb1
Stein Petter Tokvam <stto1210@gmail.com>
2024-11-20 09:20:09 +0100

    test

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('/favicon.ico', (req, res) => {
+    res.sendStatus(204);
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
