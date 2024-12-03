8954c90b848954bd9eaaa72f1193771586d4ef23
Stein Petter Tokvam <stto1210@gmail.com>
2024-12-03 08:03:03 +0100

    fundingpartner and yield bb

--- routes/barebitcoin.js
+++ routes/barebitcoin.js
@@ -1,164 +1,193 @@
-var express = require('express');
-const res = require('express/lib/response');
-var createError = require('http-errors');
+var express = require("express");
+const res = require("express/lib/response");
+var createError = require("http-errors");
 var router = express.Router();
 
 function setType(type) {
-    switch (type) {
-        case "FIAT_DEPOSIT":
-        case "BTC_DEPOSIT":
-            return "DEPOSIT";
-        case "FIAT_WITHDRAWAL":
-        case "BTC_WITHDRAWAL":
-            return "WITHDRAWAL";
-        case "BTC_BUY":
-            return "BUY";
-        case "BTC_BONUS":
-            return "YIELD";
-        case "BTC_SELL":
-            return "SELL";
-        default:
-            return type;
-    }
+  switch (type) {
+    case "FIAT_DEPOSIT":
+    case "BTC_DEPOSIT":
+      return "DEPOSIT";
+    case "FIAT_WITHDRAWAL":
+    case "BTC_WITHDRAWAL":
+      return "WITHDRAWAL";
+    case "BTC_BUY":
+      return "BUY";
+    case "BTC_BONUS":
+      return "YIELD";
+    case "BTC_SELL":
+      return "SELL";
+    default:
+      return type;
+  }
 }
 
 function setEquityShare(item) {
-    if (item.type === "BTC_BUY") {
-        return parseFloat(item.inAmount)
-    }
-    else if (item.type === "BTC_SELL") {
-        return parseFloat(item.outAmount)
-    } else if (item.type === "BTC_WITHDRAWAL") {
-        return parseFloat(item.outAmount)
-    } else if (item.type === "BTC_DEPOSIT") {
-        return parseFloat(item.inAmount)
-    } else if (item.type === "BTC_BONUS") {
-        return parseFloat(item.inAmount)
-    }
+  if (item.type === "BTC_BUY") {
+    return parseFloat(item.inAmount);
+  } else if (item.type === "BTC_SELL") {
+    return parseFloat(item.outAmount);
+  } else if (item.type === "BTC_WITHDRAWAL") {
+    return parseFloat(item.outAmount);
+  } else if (item.type === "BTC_DEPOSIT") {
+    return parseFloat(item.inAmount);
+  } else if (item.type === "BTC_BONUS") {
+    return parseFloat(item.inAmount);
+  }
 }
 
 function setFeeAmount(item) {
-    if (item.type === "BTC_WITHDRAWAL") {
-        return parseFloat(parseFloat(item.feeAmount * item.rateMarket).toFixed(2))
-    }
-    return item.feeAmount !== '' ? parseFloat(item.feeAmount) : 0
+  if (item.type === "BTC_WITHDRAWAL") {
+    return parseFloat(parseFloat(item.feeAmount * item.rateMarket).toFixed(2));
+  }
+  return item.feeAmount !== "" ? parseFloat(item.feeAmount) : 0;
 }
 
 function setCost(item) {
-    if (item.type === "BTC_WITHDRAWAL") {
-        return parseFloat(parseFloat(item.outAmount * item.rateMarket).toFixed(2))
-    } else if (item.type === "BTC_DEPOSIT") {
-        return parseFloat(parseFloat(item.inAmount * item.rateMarket).toFixed(2))
-    } else if (item.type === "BTC_BUY") {
-        return parseFloat(parseFloat(item.outAmount).toFixed(2))
-    } else if (item.type === "BTC_BONUS") {
-        return parseFloat((parseFloat(item.rateMarket) * parseFloat(item.inAmount)).toFixed(2))
-    }
-    return parseFloat(item.inAmount)
+  if (item.type === "BTC_WITHDRAWAL") {
+    return parseFloat(parseFloat(item.outAmount * item.rateMarket).toFixed(2));
+  } else if (item.type === "BTC_DEPOSIT") {
+    return parseFloat(parseFloat(item.inAmount * item.rateMarket).toFixed(2));
+  } else if (item.type === "BTC_BUY") {
+    return parseFloat(parseFloat(item.outAmount).toFixed(2));
+  } else if (item.type === "BTC_BONUS") {
+    return parseFloat(
+      (parseFloat(item.rateMarket) * parseFloat(item.inAmount)).toFixed(2)
+    );
+  }
+  return parseFloat(item.inAmount);
 }
 
 function getPrice(onlyPrice) {
-    return fetch('https://barebitcoin.no/connect/bb.v1alpha.BBService/Price', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(response => onlyPrice ? response.midBtcnok : response)
+  return fetch("https://barebitcoin.no/connect/bb.v1alpha.BBService/Price", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => (onlyPrice ? response.midBtcnok : response));
 }
 
 function getTransactions(accessKey, accountKey) {
-    return fetch('https://api.bb.no/export/transactions', {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessKey}`
-        }
-    })
-        .then(response => response.json())
-        .then(response => {
-            if (response.error) {
-                res.status(500).send(response)
-                return
-            }
-            return response
-                .filter(item => item.inCurrency !== "NOK")
-                .filter(item => item.accountId !== "acc_01J631DK3N56K40P6NC1HZXWBQ")
-                .map(item => {
-                    return {
-                        //bb_type: 'trade',
-                        transactionKey: item.id,
-                        accountKey,
-                        cost: setCost(item),
-                        name: item.type === "BTC_WITHDRAWAL" ? item.outCurrency : item.inCurrency,
-                        type: setType(item.type),
-                        date: item.createTime,
-                        equityPrice: parseFloat(item.rateMarket),
-                        e24Key: "",
-                        equityShare: setEquityShare(item),
-                        equityType: "Cryptocurrency",
-                        fee: setFeeAmount(item)
-                    };
-                });
-        })
+  return fetch("https://api.bb.no/export/transactions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessKey}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.error) {
+        res.status(500).send(response);
+        return;
+      }
+      return response
+        .filter((item) => item.inCurrency !== "NOK")
+        .filter((item) => item.accountId !== "acc_01J631DK3N56K40P6NC1HZXWBQ")
+        .map((item) => {
+          return {
+            //bb_type: 'trade',
+            transactionKey: item.id,
+            accountKey,
+            cost: setCost(item),
+            name:
+              item.type === "BTC_WITHDRAWAL"
+                ? item.outCurrency
+                : item.inCurrency,
+            type: setType(item.type),
+            date: item.createTime,
+            equityPrice: parseFloat(item.rateMarket),
+            e24Key: "",
+            equityShare: setEquityShare(item),
+            equityType: "Cryptocurrency",
+            fee: setFeeAmount(item),
+          };
+        });
+    });
 }
 
-router.get('/price', function (req, res, next) {
-    getPrice(false)
-        .then(response => res.send(response))
+router.get("/price", function (req, res, next) {
+  getPrice(false).then((response) => res.send(response));
 });
 
-router.post('/balance', function (req, res, next) {
-    const { accessKey, accountKey } = req.body
-    if (accessKey === undefined) {
-        return next(createError(400, 'Missing required fields accessKey or accountKey'))
-    }
-    fetch('https://api.bb.no/export/balance', {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessKey}`
-        }
-    })
-        .then(response => response.json())
-        .then(response => {
-            getPrice(true)
-                .then(price => {
-                    getTransactions(accessKey, accountKey)
-                        .then(transactions => {
-                            res.send(response.bitcoinAccounts.filter(res => res.name === "Hovedkonto").map(account => {
-                                const currentValue = price * parseFloat(account.balanceBitcoin)
-                                return {
-                                    name: "BTC",
-                                    accountKey: accountKey,
-                                    equityShare: parseFloat(account.balanceBitcoin),
-                                    equityType: "Cryptocurrency",
-                                    value: currentValue,
-                                    goalPercentage: 0,
-                                    yield: currentValue - transactions.reduce((a, b) => {
-                                        if (b.type === "SELL") {
-                                            return a - b.cost
-                                        } else if (b.type === "BUY") {
-                                            return a + b.cost
-                                        } else {
-                                            return a
-                                        }
-                                    }, 0),
-                                }
-                            }))
-                        })
-                })
-        })
-})
+router.post("/balance", function (req, res, next) {
+  const { accessKey, accountKey } = req.body;
+  if (accessKey === undefined) {
+    return next(
+      createError(400, "Missing required fields accessKey or accountKey")
+    );
+  }
+  fetch("https://api.bb.no/export/balance", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessKey}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      getPrice(true).then((price) => {
+        getTransactions(accessKey, accountKey).then((transactions) => {
+          res.send(
+            response.bitcoinAccounts
+              .filter((res) => res.name === "Hovedkonto")
+              .map((account) => {
+                const currentValue = price * parseFloat(account.balanceBitcoin);
+                return {
+                  name: "BTC",
+                  accountKey: accountKey,
+                  equityShare: parseFloat(account.balanceBitcoin),
+                  equityType: "Cryptocurrency",
+                  value: currentValue,
+                  goalPercentage: 0,
+                  yield:
+                    currentValue -
+                    transactions.reduce((a, b) => {
+                      if (b.type === "SELL") {
+                        return a - b.cost;
+                      } else if (b.type === "BUY") {
+                        return a + b.cost;
+                      } else {
+                        return a;
+                      }
+                    }, 0),
+                };
+              })
+          );
+        });
+      });
+    });
+});
 
-router.post('/transactions', function (req, res, next) {
-    const { accessKey, accountKey } = req.body
-    if (accessKey === undefined) {
-        return next(createError(400, 'Missing required fields accessKey'))
+router.post("/transactions", function (req, res, next) {
+  const { accessKey, accountKey } = req.body;
+  if (accessKey === undefined) {
+    return next(createError(400, "Missing required fields accessKey"));
+  }
+  getTransactions(accessKey, accountKey).then((transactions) => {
+    res.send(transactions);
+  });
+});
+
+router.post("/yield", function (req, res, next) {
+  const { accessKey } = req.body;
+  if (accessKey === undefined) {
+    return next(createError(400, "Missing required fields accessKey"));
+  }
+  fetch(
+    "https://barebitcoin.no/connect/bb.pnl.v1.PnlService/BitcoinHoldings?connect=v1&encoding=json&message=%7B%22accountId%22%3A%22acc_01HQ1MA420JN3NB2G7GWKQCPV2%22%7D",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessKey}`,
+      },
     }
-    getTransactions(accessKey, accountKey)
-        .then(transactions => {
-            res.send(transactions)
-        })
-})
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      res.send({yield: response.allTime[response.allTime.length - 1].pnlAbsolute});
+    });
+});
 
 module.exports = router;
--- routes/fundingpartner.js
+++ routes/fundingpartner.js
@@ -1,99 +1,144 @@
-var express = require('express');
-const res = require('express/lib/response');
-var createError = require('http-errors');
+var express = require("express");
+const res = require("express/lib/response");
+var createError = require("http-errors");
 var router = express.Router();
 
-async function login(userId, pw) {
-    return fetch('https://fundingpartner.no/api/v2/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            ApplicantUsername: userId,
-            ApplicantPassword: pw
+async function getTransactions(accountKey, token) {
+  return fetch(
+    "https://fundingpartner.no/api/v2/transactions-unified?startDate=2018-01-01&endDate=2024-11-29&lastILTID=&lastIFTID=",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `token=${token}`,
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const filteredTransactions = data.message.payload.transList.filter(
+        (item) =>
+          item.classification !== "depositDomestic" || // innskudd til klientkonto
+          item.classification !== "withdrawalRequestedByUser" || // manuelt uttak
+          item.classification !== "withdrawalDomesticForced" // automatisk uttak
+      );
+
+      return filteredTransactions
+        .map((item) => {
+          const transactions = [];
+          if (item.principal !== null && item.principal !== 0) {
+            transactions.push({
+              transactionKey: `${item.id}_principal`,
+              accountKey,
+              cost: -item.principal,
+              name: `${item.borrowerName} - (${item.loanId})`,
+              type: "SELL",
+              date: item.transactionDate,
+              equityPrice: 1,
+              e24Key: "",
+              equityShare: 1,
+              equityType: "Loan",
+              fee: 0,
+            });
+          }
+          if (item.netInterest !== null) {
+            transactions.push({
+              transactionKey: `${item.id}_interest`,
+              accountKey,
+              cost: item.netInterest,
+              name: `${item.borrowerName} - (${item.loanId})`,
+              type: "YIELD",
+              date: item.transactionDate,
+              equityPrice: 1,
+              e24Key: "",
+              equityShare: 1,
+              equityType: "Loan",
+              fee: 0,
+            });
+          }
+          if (item.classification === "withdrawalActiveLoan") {
+            transactions.push({
+              transactionKey: `${item.id}_buy`,
+              accountKey,
+              cost: Math.abs(item.amount),
+              name: `${item.borrowerName} - (${item.loanId})`,
+              type: "BUY",
+              date: item.transactionDate,
+              equityPrice: 1,
+              e24Key: "",
+              equityShare: 1,
+              equityType: "Loan",
+              fee: 0,
+            });
+          }
+
+          return transactions;
         })
-    }).then(res => res.json()).then(data => data.token);
+        .flat();
+    });
 }
 
-router.post('/transactions', function (req, res, next) {
-    const { userId, pw, accountKey } = req.body
-    if (userId === undefined || pw === undefined) {
-        return next(createError(400, 'Missing required fields userId or pw'))
-    }
-    login(userId, pw)
-    .then(token => {
-        fetch('https://fundingpartner.no/api/v2/transactions-unified?startDate=2018-01-01&endDate=2024-11-29&lastILTID=&lastIFTID=', {
-            headers: {
-                'Content-Type': 'application/json',
-                'Cookie': `token=${token}`
-            }
-        }).then(res => res.json()).then(data => {
-            const filteredTransactions = data.message.payload.transList
-            .filter(item => item.classification !== "depositDomestic" // innskudd til klientkonto
-                || item.classification !== "withdrawalRequestedByUser"  // manuelt uttak
-                || item.classification !== "withdrawalDomesticForced" // automatisk uttak
-            )
+async function login(userId, pw) {
+  return fetch("https://fundingpartner.no/api/v2/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      ApplicantUsername: userId,
+      ApplicantPassword: pw,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => data.token);
+}
 
-            const convertedTransactions = filteredTransactions.map(item => {
-                const transactions = []
-                if(item.principal !== null && item.principal !== 0) {
-                    transactions.push(
-                        {
-                            transactionKey: `${item.id}_principal`,
-                            accountKey,
-                            cost: -item.principal,
-                            name: `${item.borrowerName} - (${item.loanId})`,
-                            type: "SELL",
-                            date: item.transactionDate,
-                            equityPrice: 1,
-                            e24Key: "",
-                            equityShare: 1,
-                            equityType: "Loan",
-                            fee: 0
-                        }
-                    )
-                }
-                if(item.netInterest !== null) {
-                    transactions.push(
-                        {
-                            transactionKey: `${item.id}_interest`,
-                            accountKey,
-                            cost: item.netInterest,
-                            name: `${item.borrowerName} - (${item.loanId})`,
-                            type: "YIELD",
-                            date: item.transactionDate,
-                            equityPrice: 1,
-                            e24Key: "",
-                            equityShare: 1,
-                            equityType: "Loan",
-                            fee: 0
-                        }
-                    )
-                }
-                if(item.classification === "withdrawalActiveLoan") {
-                    transactions.push(
-                        {
-                            transactionKey: `${item.id}_buy`,
-                            accountKey,
-                            cost: Math.abs(item.amount),
-                            name: `${item.borrowerName} - (${item.loanId})`,
-                            type: "BUY",
-                            date: item.transactionDate,
-                            equityPrice: 1,
-                            e24Key: "",
-                            equityShare: 1,
-                            equityType: "Loan",
-                            fee: 0
-                        }
-                    )
-                }
+router.post("/transactions", function (req, res, next) {
+  const { userId, pw, accountKey } = req.body;
+  if (userId === undefined || pw === undefined) {
+    return next(createError(400, "Missing required fields userId or pw"));
+  }
+  login(userId, pw).then((token) =>
+    getTransactions(accountKey, token).then((transactions) =>
+      res.send(transactions)
+    )
+  );
+});
 
-                return transactions
-            })
-            res.send(convertedTransactions.flat())
-        })
-    });
-})
+router.post("/holdings", function (req, res, next) {
+  const { userId, pw, accountKey } = req.body;
+  if (userId === undefined || pw === undefined) {
+    return next(createError(400, "Missing required fields userId or pw"));
+  }
+  login(userId, pw).then((token) =>
+    getTransactions(accountKey, token).then((transactions) => {
+      const uniqueTransactionNames = [...new Set(
+        transactions.map((item) => item.name)
+      )];
+      const holdings = uniqueTransactionNames.map((item) => {
+        const filteredTransactions = transactions.filter(
+          (transaction) => transaction.name === item
+        );
+        const totalCost = filteredTransactions
+          .filter((item) => item.type === "BUY" || item.type === "SELL")
+          .map((item) => item.cost)
+          .reduce((a, b) => a + b, 0);
+        const totalYield = filteredTransactions
+          .filter((item) => item.type === "YIELD")
+          .map((item) => item.cost)
+          .reduce((a, b) => a + b, 0);
+        return {
+          name: item,
+          accountKey: accountKey,
+          equityShare: 1,
+          equityType: "Loan",
+          value: totalCost,
+          goalPercentage: 0,
+          yield: totalYield,
+        };
+      });
+      res.send(holdings);
+    })
+  );
+});
 
 module.exports = router;
